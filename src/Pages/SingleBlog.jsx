fa3b401dbc8c7704f08a756c32561c38d2f18fa5
Md Rashed Ali <tools.rashed@gmail.com>
2023-10-09 18:01:26 +0600

    single blog page responsvie

--- src/Pages/SingleBlog.jsx
+++ src/Pages/SingleBlog.jsx
@@ -15,19 +15,19 @@ const SingleBlog = () => {
   console.log(blog[0]?.id);
   return (
     <div className="p-[5%]">
-      <div className="blog-container grid grid-cols-3 ">
-        <div className="sidebar ">
+      <div className="blog-container grid lg:grid-cols-3 ">
+       
+        <div className="blog-post lg:order-2 col-span-2 ">
+          <div className="image">
+            <img src={`${blog[0]?.image}`} alt="" />
+          </div>
+          <h1 className="text-2xl mt-5">{blog[0]?.title}</h1>
+          <p className="mb-5"> by - {blog[0]?.author}</p>
+          <p>{blog[0]?.content}</p>
+        </div>
+        <div className="sidebar lg:order-1 mt-10 mb-10 lg:mt-0 ">
           <h1>This is sidebar</h1>
         </div>
-        <div className="blog-post  col-span-2 ">
-            <div className="image">
-                <img src={`${blog[0]?.image}`} alt="" />
-            </div>
-            <h1 className="text-2xl mt-5">{blog[0]?.title}</h1>
-            <p className="mb-5"> by - {blog[0]?.author}</p>
-            <p>{blog[0]?.content}</p>
-            </div>
-            
       </div>
     </div>
   );
