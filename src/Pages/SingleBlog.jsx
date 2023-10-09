91094fa72c4d272faa4726688753898dac0e8bf4
Md Rashed Ali <tools.rashed@gmail.com>
2023-10-09 20:11:02 +0600

    single blog design update

--- src/Pages/SingleBlog.jsx
+++ src/Pages/SingleBlog.jsx
@@ -1,21 +1,24 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import { fetchBlog } from "../Hooks/fetchBlogs";
+import {BiSearchAlt2} from 'react-icons/bi'
 
 const SingleBlog = () => {
   const id = useParams();
   const [blog, setBlog] = useState([]);
+  const [allBlogs, setAllBlogs] = useState([])
 
   useEffect(() => {
     fetchBlog().then((blogs) => {
+      setAllBlogs(blogs.blogPosts)
       let filterBlog = blogs.blogPosts.filter((blog) => blog.id == id.id);
       setBlog(filterBlog);
     });
   }, [id]);
-  console.log(blog[0]?.id);
+  console.log(allBlogs);
   return (
     <div className="p-[5%]">
-      <div className="blog-container grid lg:grid-cols-3 ">
+      <div className="blog-container grid lg:grid-cols-3 gap-8">
        
         <div className="blog-post lg:order-2 col-span-2 ">
           <div className="image">
@@ -25,8 +28,23 @@ const SingleBlog = () => {
           <p className="mb-5"> by - {blog[0]?.author}</p>
           <p>{blog[0]?.content}</p>
         </div>
-        <div className="sidebar lg:order-1 mt-10 mb-10 lg:mt-0 ">
-          <h1>This is sidebar</h1>
+        <div className="sidebar col-span-2 lg:col-span-1 flex flex-col gap-5 justify-center lg:justify-normal lg:order-1 mt-10 mb-10 lg:mt-0 ">
+          <div className="search w-full relative">
+            <input placeholder="Search here..." className="w-full h-12 px-3 py-2 border-2 border-color-secondary outline-color-primary" type="text" />
+            <p className=" absolute bg-color-secondary rounded-full p-2 text-xl flex justify-center items-center text-[#fff] right-2 bottom-[6px]"><BiSearchAlt2></BiSearchAlt2></p>
+          </div>
+          <div>
+            <div><h1 className="text-2xl mb-4">Latest Post</h1></div>
+            <div className="space-y-3">
+              {
+                allBlogs.map((oneBlog)=>(
+                  <ul  key={oneBlog.id}>
+                   <Link className="hover:text-color-secondary transition-all ease-in-out" to={`/blogs/${oneBlog.id}`}><li>{oneBlog?.title}</li></Link> 
+                  </ul>
+                ))
+              }
+            </div>
+          </div>
         </div>
       </div>
     </div>
