3db7da35468efb9e766b29174b206114611aed86
Md Rashed Ali <tools.rashed@gmail.com>
2023-10-09 21:52:37 +0600

    footer desing improved

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,6 +1,8 @@
 import { Link } from "react-router-dom";
 import FooterBg from "../../assets/footer_bg.jpg";
 import Logo from "/public/logo.png";
+import {FaFacebookF, FaInstagram} from "react-icons/fa";
+import {RiTwitterXFill, RiLinkedinFill} from "react-icons/ri";
 const Footer = () => {
   return (
     <div className="">
@@ -12,9 +14,23 @@ const Footer = () => {
           backgroundRepeat: "no-repeat",
         }}
       >
-        <aside>
+        <aside className="flex flex-col justify-center items-center">
           <img className="h-16" src={Logo} alt="" />
-          <div className="social"></div>
+          <div className="social hidden lg:flex gap-3 mt-5 justify-between items-center">
+            <a className="bg-color-primary p-3 text-xl text-color-main rounded-full" href="#">
+              <FaFacebookF></FaFacebookF>
+            </a>
+            <a className="bg-color-primary p-3 text-xl text-color-main rounded-full" href="#">
+              <RiTwitterXFill></RiTwitterXFill>
+            </a>
+            <a className="bg-color-primary p-3 text-xl text-color-main rounded-full" href="#">
+              <RiLinkedinFill></RiLinkedinFill>
+            </a>
+            <a className="bg-color-primary p-3 text-xl text-color-main rounded-full" href="#">
+              <FaInstagram></FaInstagram>
+            </a>
+            
+          </div>
         </aside>
         <nav className="font-bold hidden lg:grid">
           <header className="text-xl flex justify-center">Pages</header>
@@ -25,7 +41,7 @@ const Footer = () => {
           <Link to="/contact-us"><a className="link link-hover">Contact</a></Link>
           
         </nav>
-        <nav className="font-bold">
+        <nav className="font-bold hidden lg:grid">
           <header className="text-xl">Services</header>
         <Link to="/services/1">  <a className="link link-hover">Wedding Planning</a></Link>
         <Link to="/services/2">  <a className="link link-hover">Birthday Party</a></Link>
@@ -46,6 +62,25 @@ const Footer = () => {
             <span>Email: </span>demo@gmail.com
           </a>
         </nav>
+        <div className="social lg:hidden flex flex-col gap-3 mt-5 justify-between items-center">
+          <h1 className="text-xl">Visit Social</h1>
+          <div className="flex gap-3 justify-between items-center">
+          <a className="bg-color-primary p-3 text-xl text-color-main rounded-full" href="#">
+              <FaFacebookF></FaFacebookF>
+            </a>
+            <a className="bg-color-primary p-3 text-xl text-color-main rounded-full" href="#">
+              <RiTwitterXFill></RiTwitterXFill>
+            </a>
+            <a className="bg-color-primary p-3 text-xl text-color-main rounded-full" href="#">
+              <RiLinkedinFill></RiLinkedinFill>
+            </a>
+            <a className="bg-color-primary p-3 text-xl text-color-main rounded-full" href="#">
+              <FaInstagram></FaInstagram>
+            </a>
+            
+          </div>
+           
+          </div>
       </footer>
     </div>
   );
